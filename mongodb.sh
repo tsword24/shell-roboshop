454ca8ce4243db9f6f7a5858b157a1558e2872d0
khajit <ssvvkhajit@gmail.com>
2025-10-14 00:07:19 +0530

    shell-roboshop

---
+++ mongodb.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+USERID=$(id -u)
+
+LOGS_FOLDER="/var/log/mongodb"
+SCRIPT_NAME="$( echo $0 | cut -d "." -f1)" #to get the script name
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script Started at $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then 
+    echo "Give root privilages"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ];then
+        echo -e "Install is $G success$N"  | tee -a $LOG_FILE
+    else
+        echo -e "$R Failure$N" | tee -a $LOG_FILE
+    fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Adding mongo repo" 
+
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "Installing mongodb"
+
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "Enabling mongodb"
+
+systemctl start mongod 
+VALIDATE $? "Starting mongodb"
