60bc35e18eae6c3b9ac970a64213a990500b7611
khajit <ssvvkhajit@gmail.com>
2025-10-14 22:51:01 +0530

    mongodb shell

--- mongodb.sh
+++ mongodb.sh
@@ -42,5 +42,5 @@ VALIDATE $? "Starting mongodb"
 sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mongod.conf
 VALIDATE $? "Allowing remote connection to mongodb"
 
-systemctl restart mongodb
+systemctl restart mongod
 VALIDATE $? "Restarting mongobd"
