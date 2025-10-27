477baa376d4907da525f84bc07fd2f2384c98607
khajit <ssvvkhajit@gmail.com>
2025-10-27 23:24:00 +0530

    redis

---
+++ redis.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+USERID=$(id -u)
+
+LOGS_FOLDER="/var/log/mongodb"
+SCRIPT_NAME="$( echo $0 | cut -d "." -f1)" #to get the script name
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+MONGODB_DOMAIN="mongodb.ssnationals.fun"
+DIRECTORY=$PWD
+$START_TIME=$(date +%s)
+mkdir -p $LOGS_FOLDER
+echo "Script Started at $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then 
+    echo "Give root privilages"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ];then
+        echo -e " $2 $G success$N"  | tee -a $LOG_FILE
+    else
+        echo -e " $2 $R Failure$N" | tee -a $LOG_FILE
+        exit 1 
+    fi
+}
+
+dnf module disable redis -y
+VALIDATE $? "Disable  redis"
+dnf module enable redis:7 -y
+VALIDATE $? "Enabling redis 7"
+dnf install redis -y
+VALIDATE $? "Intstalling redis" 
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Changing redis files"
+
+systemctl enable redis 
+VALIDATE $? "Enabling redis"
+
+systemctl start redis 
+VALIDATE $? "Starting redis"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME))
+echo -e "Script executed in : $Y $TOTAL_TIME $N"
