061d8a98f63385fcb9f79f8c33d632915c3d941a
khajit <ssvvkhajit@gmail.com>
2025-10-27 23:32:56 +0530

    redis

--- redis.sh
+++ redis.sh
@@ -29,20 +29,20 @@ VALIDATE(){
     fi
 }
 
-dnf module disable redis -y
+dnf module disable redis -y &>>$LOG_FILE
 VALIDATE $? "Disable  redis"
-dnf module enable redis:7 -y
+dnf module enable redis:7 -y &>>$LOG_FILE
 VALIDATE $? "Enabling redis 7"
-dnf install redis -y
+dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "Intstalling redis" 
 
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 VALIDATE $? "Changing redis files"
 
-systemctl enable redis 
+systemctl enable redis &>>$LOG_FILE
 VALIDATE $? "Enabling redis"
 
-systemctl start redis 
+systemctl start redis &>>$LOG_FILE
 VALIDATE $? "Starting redis"
 
 END_TIME=$(date +%s)
